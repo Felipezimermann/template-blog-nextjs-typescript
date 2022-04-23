0abd08f9d10560178f706f6fa8ef64cf02bdda63
FELIPE ANDRADE ZIMERMANN <felipe-zimerman-n@hotmail.com>
2022-04-23 18:26:15 -0300

    alteraçoes no header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,31 @@
+import Image from "next/image"
+
 export default  ()=>{
     return(
         <>
           <div className="header">
-              ola helena zimermannn ts
+              <span><h2>Game of Trones</h2></span>
+              <nav className="header_menu">
+                 <ul>
+                    <a>Home</a>
+                 </ul>
+                 <ul>
+                    <a>Sobre</a>
+                 </ul>
+                 <ul>
+                    <a>Contato</a>
+                 </ul>
+              </nav>
           </div>
+          <div className="header_img">
+            <Image 
+            src="/teste.jpg"
+            layout="responsive"
+            height={30}
+            width={100}
+            />
+          </div>
+        
         </>
     )
 }
