4bbea41d4f9416414e24f5a05645ce8dd732a90f
FELIPE ANDRADE ZIMERMANN <felipe-zimerman-n@hotmail.com>
2022-04-23 11:09:35 -0300

    teste

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,11 +2,7 @@ export default  ()=>{
     return(
         <>
           <div className="header">
-<<<<<<< HEAD
               ola felipe tudo bem 
-=======
-              ola tu esta
->>>>>>> main
           </div>
         </>
     )
