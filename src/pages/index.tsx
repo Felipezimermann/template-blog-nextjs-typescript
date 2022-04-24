78b59043f3f0d6cc6c94c3aac3b96f3fa582dcdc
FELIPE ANDRADE ZIMERMANN <felipe-zimerman-n@hotmail.com>
2022-04-24 02:18:16 -0300

    new settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,31 +5,31 @@ export default function index(){
     <>
       <main className={style.main}>
           <section className={style.main_section}>
-            <h3>Ameca, o robô humanoide que chamou atenção na CES 2022</h3>
+            <h3>Ameca, the humanoid robot that drew attention at CES 2022</h3>
             <p>
-              Projetada para hospedar inteligência artificial, a criação da Engineered Arts surpreendeu visitantes da maior feira de tecnologia do mundo por suas expressões realistas e capacidade de comunicação; veja vídeos
+            Designed to host artificial intelligence, Engineered Arts' creation surprised visitors to the world's largest technology fair for its realistic expressions and communication skills; watch videos
             </p>
             <img  style={{width:'50vw'}} src="https://classic.exame.com/wp-content/uploads/2022/01/amecaengineeredarts.jpg?quality=70&strip=info&w=1024"></img>
             <p>
-              O futuro ao lado dos robôs humanoide está mais perto do que nunca. Após um vídeo seu viralizar em dezembro de 2021 (com direito a um "eca" de Elon Musk), o robô Ameca da britânica Engineered Arts foi um dos assuntos mais falados durante a CES 2022, maior feira de tecnologia e inovação do mundo, que aconteceu nesta última semana.
+              The future alongside humanoid robots is closer than ever. After a video of it went viral in December 2021 (with an "yuck" from Elon Musk), the British Engineered Arts robot Ameca was one of the most talked about subjects during CES 2022, the largest technology and innovation fair in the world, which happened this last week.
             </p>
             <p>
-              Diferente de robôs como os da Boston Dynamics, que dançam coreografias completas, o Ameca não corre ou pula. Tampouco é um ajudante doméstico como os idealizados pela Toyota, Samsung e Amazon. Ameca chama atenção pelas suas expressões faciais realistas e capacidade de conversar sozinha — no estande da sua empresa, o próprio robô era responsável por explicar o que era.
+              Unlike robots like Boston Dynamics, which dance full-length choreographies, Ameca doesn't run or jump. Nor is it a domestic helper like those envisioned by Toyota, Samsung and Amazon. Ameca stands out for its realistic facial expressions and ability to talk to itself — in her company's booth, the robot itself was responsible for explaining what it was.
             </p>
             <p>
-              O corpo acinzentado de metal e plástico remove um pouco do aspecto humano, mas seu rosto traz expressões faciais extremamente vívidas (e até dentes).
+              The grayish metal and plastic body removes some of the human aspect, but his face brings out extremely vivid facial expressions (and even teeth).
             </p>
             <p>
-              Para obter tal efeito, a Engineered Arts colocou 17 motores individuais na cabeça do robô, construídos a partir de scans 3D internos de pessoas reais. Assim, foi possível imitar não só as expressões humanas, como também a estrutura óssea e a textura da pele.
+              To achieve this effect, Engineered Arts placed 17 individual motors in the robot's head, built from internal 3D scans of real people. Thus, it was possible to imitate not only human expressions, but also bone structure and skin texture.
             </p>
             <p>
-              O humanoide da Engineered Arts foi projetado como uma plataforma para hospedar inteligência artificial (IA), mas não há nada embutido ainda. Por enquanto, ele conta somente com essa série de movimentos pré-programados, feitos para simular o movimento humano.
+              Engineered Arts' humanoid was designed as a platform to host artificial intelligence (AI), but there's nothing built in yet. For now, he has only this series of pre-programmed movements, designed to simulate human movement.
             </p>
             <p>
-              Outro fato interessante é que o Ameca foi projetado para ser modular, o que significa que a cabeça pode ser removida com um clique e sem ferramentas, permitindo que seja trocada por outra caso algo dê errado.
+              Another interesting fact is that the Ameca is designed to be modular, which means that the head can be removed with a click and without tools, allowing it to be swapped out in case something goes wrong.
             </p>
             <p>
-              Por enquanto, os planos da empresa Engineered Arts são de continuar desenvolvendo o robô humanoide. Ameca deve ter um IA em breve, e há planos de novas versões na qual ele é capaz de andar. Esperamos que não para muito longe.
+              For now, Engineered Arts plans to continue developing the humanoid robot. Ameca should have an AI soon, and there are plans for new versions of it that he's capable of riding on. We hope it doesn't go too far.
             </p>
           </section>         
       </main>
